0f4bb7d080f3ecaf6b6ea17fa46b6fa82c2183b2
programizta <koleczkowsky@gmail.com>
2019-02-14 17:11:33 +0100

    Zadanie 1 - zadanie domowe

---
+++ PiotrKoleczeklab1/GameForm.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace PiotrKoleczeklab1
+{
+    partial class GameForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.labelYourHuman = new System.Windows.Forms.Label();
+            this.labelYourTanks = new System.Windows.Forms.Label();
+            this.labelYourResources = new System.Windows.Forms.Label();
+            this.labelEnemyResources = new System.Windows.Forms.Label();
+            this.labelEnemyHuman = new System.Windows.Forms.Label();
+            this.labelEnemyTanks = new System.Windows.Forms.Label();
+            this.textBoxYourHuman = new System.Windows.Forms.TextBox();
+            this.textBoxYourTanks = new System.Windows.Forms.TextBox();
+            this.textBoxEnemyHuman = new System.Windows.Forms.TextBox();
+            this.textBoxEnemyTanks = new System.Windows.Forms.TextBox();
+            this.timerDecreaseYourResources = new System.Windows.Forms.Timer(this.components);
+            this.buttonStartGame = new System.Windows.Forms.Button();
+            this.buttonCallSupport = new System.Windows.Forms.Button();
+            this.timerDecreaseEnemyResources = new System.Windows.Forms.Timer(this.components);
+            this.buttonCallArtillery = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelYourHuman
+            // 
+            this.labelYourHuman.AutoSize = true;
+            this.labelYourHuman.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelYourHuman.Location = new System.Drawing.Point(33, 139);
+            this.labelYourHuman.Name = "labelYourHuman";
+            this.labelYourHuman.Size = new System.Drawing.Size(144, 20);
+            this.labelYourHuman.TabIndex = 0;
+            this.labelYourHuman.Text = "Jednostki piechoty:";
+            // 
+            // labelYourTanks
+            // 
+            this.labelYourTanks.AutoSize = true;
+            this.labelYourTanks.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelYourTanks.Location = new System.Drawing.Point(33, 209);
+            this.labelYourTanks.Name = "labelYourTanks";
+            this.labelYourTanks.Size = new System.Drawing.Size(121, 20);
+            this.labelYourTanks.TabIndex = 1;
+            this.labelYourTanks.Text = "Liczba czołgów:";
+            // 
+            // labelYourResources
+            // 
+            this.labelYourResources.AutoSize = true;
+            this.labelYourResources.Font = new System.Drawing.Font("Comic Sans MS", 24.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelYourResources.Location = new System.Drawing.Point(29, 27);
+            this.labelYourResources.Name = "labelYourResources";
+            this.labelYourResources.Size = new System.Drawing.Size(249, 45);
+            this.labelYourResources.TabIndex = 2;
+            this.labelYourResources.Text = "Twoje zasoby:";
+            // 
+            // labelEnemyResources
+            // 
+            this.labelEnemyResources.AutoSize = true;
+            this.labelEnemyResources.Font = new System.Drawing.Font("Comic Sans MS", 24.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelEnemyResources.Location = new System.Drawing.Point(530, 27);
+            this.labelEnemyResources.Name = "labelEnemyResources";
+            this.labelEnemyResources.Size = new System.Drawing.Size(252, 45);
+            this.labelEnemyResources.TabIndex = 3;
+            this.labelEnemyResources.Text = "Zasoby wroga:";
+            // 
+            // labelEnemyHuman
+            // 
+            this.labelEnemyHuman.AutoSize = true;
+            this.labelEnemyHuman.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelEnemyHuman.Location = new System.Drawing.Point(534, 139);
+            this.labelEnemyHuman.Name = "labelEnemyHuman";
+            this.labelEnemyHuman.Size = new System.Drawing.Size(144, 20);
+            this.labelEnemyHuman.TabIndex = 4;
+            this.labelEnemyHuman.Text = "Jednostki piechoty:";
+            // 
+            // labelEnemyTanks
+            // 
+            this.labelEnemyTanks.AutoSize = true;
+            this.labelEnemyTanks.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelEnemyTanks.Location = new System.Drawing.Point(534, 209);
+            this.labelEnemyTanks.Name = "labelEnemyTanks";
+            this.labelEnemyTanks.Size = new System.Drawing.Size(121, 20);
+            this.labelEnemyTanks.TabIndex = 5;
+            this.labelEnemyTanks.Text = "Liczba czołgów:";
+            // 
+            // textBoxYourHuman
+            // 
+            this.textBoxYourHuman.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxYourHuman.Location = new System.Drawing.Point(224, 136);
+            this.textBoxYourHuman.Name = "textBoxYourHuman";
+            this.textBoxYourHuman.Size = new System.Drawing.Size(132, 26);
+            this.textBoxYourHuman.TabIndex = 6;
+            // 
+            // textBoxYourTanks
+            // 
+            this.textBoxYourTanks.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxYourTanks.Location = new System.Drawing.Point(224, 206);
+            this.textBoxYourTanks.Name = "textBoxYourTanks";
+            this.textBoxYourTanks.Size = new System.Drawing.Size(132, 26);
+            this.textBoxYourTanks.TabIndex = 8;
+            // 
+            // textBoxEnemyHuman
+            // 
+            this.textBoxEnemyHuman.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxEnemyHuman.Location = new System.Drawing.Point(693, 136);
+            this.textBoxEnemyHuman.Name = "textBoxEnemyHuman";
+            this.textBoxEnemyHuman.Size = new System.Drawing.Size(132, 26);
+            this.textBoxEnemyHuman.TabIndex = 9;
+            // 
+            // textBoxEnemyTanks
+            // 
+            this.textBoxEnemyTanks.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxEnemyTanks.Location = new System.Drawing.Point(693, 206);
+            this.textBoxEnemyTanks.Name = "textBoxEnemyTanks";
+            this.textBoxEnemyTanks.Size = new System.Drawing.Size(132, 26);
+            this.textBoxEnemyTanks.TabIndex = 10;
+            // 
+            // timerDecreaseYourResources
+            // 
+            this.timerDecreaseYourResources.Tick += new System.EventHandler(this.timerDecreaseYourResources_Tick);
+            // 
+            // buttonStartGame
+            // 
+            this.buttonStartGame.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonStartGame.Location = new System.Drawing.Point(386, 263);
+            this.buttonStartGame.Name = "buttonStartGame";
+            this.buttonStartGame.Size = new System.Drawing.Size(116, 44);
+            this.buttonStartGame.TabIndex = 11;
+            this.buttonStartGame.Text = "Zacznij grę!";
+            this.buttonStartGame.UseVisualStyleBackColor = true;
+            this.buttonStartGame.Click += new System.EventHandler(this.buttonStartGame_Click);
+            // 
+            // buttonCallSupport
+            // 
+            this.buttonCallSupport.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonCallSupport.Location = new System.Drawing.Point(12, 346);
+            this.buttonCallSupport.Name = "buttonCallSupport";
+            this.buttonCallSupport.Size = new System.Drawing.Size(128, 44);
+            this.buttonCallSupport.TabIndex = 12;
+            this.buttonCallSupport.Text = "Wezwij posiłki";
+            this.buttonCallSupport.UseVisualStyleBackColor = true;
+            this.buttonCallSupport.Click += new System.EventHandler(this.buttonCallSupport_Click);
+            // 
+            // timerDecreaseEnemyResources
+            // 
+            this.timerDecreaseEnemyResources.Tick += new System.EventHandler(this.timerDecreaseEnemyResources_Tick);
+            // 
+            // buttonCallArtillery
+            // 
+            this.buttonCallArtillery.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonCallArtillery.Location = new System.Drawing.Point(12, 427);
+            this.buttonCallArtillery.Name = "buttonCallArtillery";
+            this.buttonCallArtillery.Size = new System.Drawing.Size(266, 51);
+            this.buttonCallArtillery.TabIndex = 13;
+            this.buttonCallArtillery.Text = "Wezwij wsparcie artyleryjskie";
+            this.buttonCallArtillery.UseVisualStyleBackColor = true;
+            this.buttonCallArtillery.Click += new System.EventHandler(this.buttonCallArtillery_Click);
+            // 
+            // GameForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(896, 549);
+            this.Controls.Add(this.buttonCallArtillery);
+            this.Controls.Add(this.buttonCallSupport);
+            this.Controls.Add(this.buttonStartGame);
+            this.Controls.Add(this.textBoxEnemyTanks);
+            this.Controls.Add(this.textBoxEnemyHuman);
+            this.Controls.Add(this.textBoxYourTanks);
+            this.Controls.Add(this.textBoxYourHuman);
+            this.Controls.Add(this.labelEnemyTanks);
+            this.Controls.Add(this.labelEnemyHuman);
+            this.Controls.Add(this.labelEnemyResources);
+            this.Controls.Add(this.labelYourResources);
+            this.Controls.Add(this.labelYourTanks);
+            this.Controls.Add(this.labelYourHuman);
+            this.Name = "GameForm";
+            this.Text = "Rozgrywka";
+            this.Load += new System.EventHandler(this.GameForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelYourHuman;
+        private System.Windows.Forms.Label labelYourTanks;
+        private System.Windows.Forms.Label labelYourResources;
+        private System.Windows.Forms.Label labelEnemyResources;
+        private System.Windows.Forms.Label labelEnemyHuman;
+        private System.Windows.Forms.Label labelEnemyTanks;
+        private System.Windows.Forms.TextBox textBoxYourHuman;
+        private System.Windows.Forms.TextBox textBoxYourTanks;
+        private System.Windows.Forms.TextBox textBoxEnemyHuman;
+        private System.Windows.Forms.TextBox textBoxEnemyTanks;
+        private System.Windows.Forms.Timer timerDecreaseYourResources;
+        private System.Windows.Forms.Button buttonStartGame;
+        private System.Windows.Forms.Button buttonCallSupport;
+        private System.Windows.Forms.Timer timerDecreaseEnemyResources;
+        private System.Windows.Forms.Button buttonCallArtillery;
+    }
+}
---
+++ PiotrKoleczeklab1/GameForm.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PiotrKoleczeklab1
+{
+    /// <summary>
+    /// Jest to klasa 
+    /// </summary>
+    public partial class GameForm : Form
+    {
+        // komunikat wyświetlany tuż przed uruchomieniem okna gry gdy wybraliśmy nację jako ZSRR
+        static string zsrrText = "Wygląda na to, że III Rzesza zaatakowała na dobre! Potężne oddziały " +
+            "składające się z wielu oddziałów piechoty, czołgów PanzerKampfwagen V Panther, PanzerKampfwagen VI Tiger " +
+            "oraz floty morskiej nacierają w Twoją stronę. Tylko uważaj! Może się zdarzyć, że wróg może zaatakować " +
+            "w stylu 'Blitzkrieg', czyli zostaną zmobilizowane wszystkie jednostki wroga i z pełną siłą zaczną niszczyć " +
+            "Twoje jednostki. Wróg posiada także możliwość ostrzału artyleryjskiego, ostrzału z floty morskiej, a także " +
+            "wezwania posiłków podniebnych w postaci myśliwców i bombowców. Powodzenia!";
+
+        // komunikat wyświetlany tuż przed uruchomieniem okna gry gdy wybraliśmy nację jako III Rzesza
+        static string reichText = "Najwyższa pora rozprawić się z ZSRR! Ruszamy w bój! Musimy się przedrzeć do przodu " +
+            "i zniszczyć wszystkie obiekty wroga. Co prawda mamy skończoną liczbę jednostek, ale mając flotę morską " +
+            "dobrze uzbrojone czołgi oraz możliwość wezwania bombowców powinniśmy dać sobie radę!";
+
+        // zmienne licznikowe, które służą do generowania w późniejszym czasie tzw. "katastrofy"
+        int yourSeconds, enemySeconds;
+        // zmienna służąca do zliczenia ile razy wezwane zostały już posiłki
+        int callResources;
+        // zmienna służąca do zliczenia ile razy wezwana została już artyleria
+        int callArtillery;
+        // zmienne przechowujące liczbę jednostek oraz czołgów dla gracza i wroga
+        int yourHumanResources, enemyHumanResources;
+        int yourTanks, enemyTanks;
+        // deklaracja zmiennej typu Rand w celu pseudolosowej zmiany zasobów gracza i przeciwnika
+        Random rand;
+
+        /// <summary>
+        /// Wszystkie TextBoxy ustawiamy tylko do odczytu, żeby użytkownik
+        /// nie mógł wprowadzać własnych liczb
+        /// </summary>
+        public GameForm()
+        {
+            InitializeComponent();
+            textBoxYourHuman.ReadOnly = true;
+            textBoxYourTanks.ReadOnly = true;
+            textBoxEnemyHuman.ReadOnly = true;
+            textBoxEnemyTanks.ReadOnly = true;
+        }
+
+        /// <summary>
+        /// Metoda ustawiająca łatwy poziom gry
+        /// </summary>
+        private void SetEasyMode()
+        {
+            yourHumanResources = 10000;
+            yourTanks = 7400;
+
+            enemyHumanResources = 6500;
+            enemyTanks = 2200;
+
+            textBoxYourHuman.Text = yourHumanResources + "";
+            textBoxYourTanks.Text = yourTanks + "";
+            textBoxEnemyHuman.Text = enemyHumanResources + "";
+            textBoxEnemyTanks.Text = enemyTanks + "";
+            timerDecreaseYourResources.Interval = 1000;
+            timerDecreaseEnemyResources.Interval = 800;
+        }
+
+        /// <summary>
+        /// Metoda ustawiająca średni poziom gry
+        /// </summary>
+        private void SetMediumMode()
+        {
+            yourHumanResources = 8000;
+            yourTanks = 6000;
+
+            enemyHumanResources = 7500;
+            enemyTanks = 3800;
+
+            textBoxYourHuman.Text = yourHumanResources + "";
+            textBoxYourTanks.Text = yourTanks + "";
+            textBoxEnemyHuman.Text = enemyHumanResources + "";
+            textBoxEnemyTanks.Text = enemyTanks + "";
+            timerDecreaseYourResources.Interval = 850;
+            timerDecreaseEnemyResources.Interval = 900;
+        }
+
+        /// <summary>
+        /// Metoda ustawiająca trudny poziom gry
+        /// </summary>
+        private void SetHardMode()
+        {
+            yourHumanResources = 4000;
+            yourTanks = 1400;
+
+            enemyHumanResources = 8000;
+            enemyTanks = 4900;
+
+            textBoxYourHuman.Text = yourHumanResources + "";
+            textBoxYourTanks.Text = yourTanks + "";
+            textBoxEnemyHuman.Text = enemyHumanResources + "";
+            textBoxEnemyTanks.Text = enemyTanks + "";
+            timerDecreaseYourResources.Interval = 700;
+            timerDecreaseEnemyResources.Interval = 950;
+        }
+
+        /// <summary>
+        /// Metoda ustawiająca ekstremalny poziom gry
+        /// </summary>
+        private void SetExtremeMode()
+        {
+            yourHumanResources = 2500;
+            yourTanks = 930;
+
+            enemyHumanResources = 10000;
+            enemyTanks = 5800;
+
+            textBoxYourHuman.Text = yourHumanResources + "";
+            textBoxYourTanks.Text = yourTanks + "";
+            textBoxEnemyHuman.Text = enemyHumanResources + "";
+            textBoxEnemyTanks.Text = enemyTanks + "";
+            timerDecreaseYourResources.Interval = 500;
+            timerDecreaseEnemyResources.Interval = 950;
+        }
+
+        /// <summary>
+        /// Metoda odpowiedzialna za wyświetlenie w messageboxie informację w przypadku wyboru nacji jako ZSRR
+        /// </summary>
+        private void ShowZSRRMessage()
+        {
+            MessageBox.Show(zsrrText);
+        }
+
+        /// <summary>
+        /// Metoda odpowiedzialna za wyświetlenie w messageboxie informację w przypadku wyboru nacji jako III Rzesza
+        /// </summary>
+        private void ShowReichText()
+        {
+            MessageBox.Show(reichText);
+        }
+
+        /// <summary>
+        /// Inicjalizacja generatora liczb pseudolosowych, ustawienie poziomu trudności
+        /// i koloru tła
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void GameForm_Load(object sender, EventArgs e)
+        {
+            rand = new Random();
+            if (FormMain.GetNation() == "reich") MessageBox.Show(reichText);
+            else MessageBox.Show(zsrrText);
+            this.BackColor = Color.Silver;
+            if (FormMain.GetDifficulty() == "easy") SetEasyMode();
+            else if (FormMain.GetDifficulty() == "medium") SetMediumMode();
+            else if (FormMain.GetDifficulty() == "hard") SetHardMode();
+            else SetExtremeMode();
+        }
+
+        /// <summary>
+        /// W tej metodzie wraz z cyklem czasomierza dochodzi do pewnych zdarzeń dla zasobów użytkownika.
+        /// Są one pomniejszane
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void timerDecreaseYourResources_Tick(object sender, EventArgs e)
+        {
+            if (yourHumanResources <= 0)
+            {
+                textBoxYourHuman.Text = "0";
+                timerDecreaseYourResources.Stop();
+                timerDecreaseEnemyResources.Stop();
+                MessageBox.Show("Twoje wszystkie jednostki piechoty zostały zabite, a czołgom skończyła się amunicja! " +
+                    "Przegrałeś!");
+                this.Close();
+            }
+            else
+            {
+                yourSeconds++;
+                if (yourSeconds == 30)
+                {
+                    if (FormMain.GetNation() == "zsrr")
+                    {
+                        MessageBox.Show("O nie! III Rzesza zmobilizowała wszystkie swoje oddziały " +
+                            "i wykonali 'Szybką Wojnę'! Straty w Twoich jednostkach są ogromne.");
+                        yourHumanResources -= rand.Next(1000, 1900);
+                        yourTanks -= rand.Next(300, 500);
+                    }
+                    else if (FormMain.GetNation() == "reich")
+                    {
+                        MessageBox.Show("ZSRR wezwało wsparcie artyleryjskie oraz przybywają bombowce. " +
+                            "Straty w Twoich jednostkach są wielkie!");
+                        yourHumanResources -= rand.Next(1000, 1900);
+                        yourTanks -= rand.Next(300, 500);
+                    }
+
+                }
+                yourHumanResources -= rand.Next(30, 100);
+                yourTanks -= rand.Next(10, 20);
+                textBoxYourHuman.Text = yourHumanResources + "";
+                textBoxYourTanks.Text = yourTanks + "";
+            }
+        }
+
+        /// <summary>
+        /// Metoda uruchamiająca czasomierze gdy klikniemy przycisk "Zacznij grę!"
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonStartGame_Click(object sender, EventArgs e)
+        {
+            timerDecreaseYourResources.Start();
+            timerDecreaseEnemyResources.Start();
+        }
+
+        /// <summary>
+        /// Metoda odpowiedzialna za wezwanie posiłków.
+        /// Zasoby ludzkie i czołgi zostają uzupełnione u gracza.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonCallSupport_Click(object sender, EventArgs e)
+        {
+            callResources++;
+            if (callResources <= 3)
+            {
+                MessageBox.Show("Posiłki są w drodze! Wnet Twoje straty w jednostkach zostaną uzupełnione");
+                yourHumanResources += 1500;
+                yourTanks += 700;
+            }
+            else
+            {
+                MessageBox.Show("Niestety, ale wykorzystałeś wszystkie swoje posiłki, które mogłeś wezwać.");
+            }
+
+        }
+
+        /// <summary>
+        /// Metoda wzywająca ostrzał artyleryjski na wroga.
+        /// Zasoby przeciwnika zostają pomniejszone.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonCallArtillery_Click(object sender, EventArgs e)
+        {
+            callArtillery++;
+            if (callArtillery <= 3)
+            {
+                MessageBox.Show("Operatorowi radiostacji udało się nawiązać kontakt ze wsparciem artyleryjskim! " +
+                    "Pociski celnie trafiają w cel i zadają spore straty u wroga!");
+                enemyHumanResources -= 1500;
+                enemyTanks -= 500;
+            }
+            else
+            {
+                MessageBox.Show("Ojoj! Radiooperatorowi nawaliła radiostacja! Nie wezwiemy już wsparcia artyleryjskiego!");
+            }
+        }
+
+        /// <summary>
+        /// Metoda, która wraz z każdym kolejnym cyklem czasomierza wywołuje pewne, korzystne dla wroga zdarzenia,
+        /// pomniejsza zasoby swoich wojsk
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void timerDecreaseEnemyResources_Tick(object sender, EventArgs e)
+        {
+            if (enemyHumanResources <= 0)
+            {
+                textBoxEnemyHuman.Text = "0";
+                timerDecreaseYourResources.Stop();
+                timerDecreaseEnemyResources.Stop();
+                MessageBox.Show("Zabiłeś wszystkie jednostki wroga, a wrogim czołgom skończyła się amunicja! " +
+                    "Wygrałeś!");
+                this.Close();
+            }
+            else
+            {
+                enemySeconds++;
+                if (enemySeconds >= 40)
+                {
+                    MessageBox.Show("Wrogowi udało się częściowo uzupełnić straty w jednostkach!");
+                    enemyHumanResources += rand.Next(150, 450);
+                    enemyTanks += rand.Next(50, 150);
+                }
+                enemyHumanResources -= rand.Next(100, 500);
+                enemyTanks -= rand.Next(12, 190);
+                textBoxEnemyHuman.Text = enemyHumanResources + "";
+                textBoxEnemyTanks.Text = enemyTanks + "";
+            }
+
+        }
+    }
+}
